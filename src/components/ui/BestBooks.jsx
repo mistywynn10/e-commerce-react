22c320740397349272a493b82f753246a86fb831
mistywynn10 <mistywynn10@gmail.com>
2026-05-22 15:49:48 -0400

    Vercel Push

--- src/components/ui/BestBooks.jsx
+++ src/components/ui/BestBooks.jsx
@@ -8,7 +8,7 @@ const BestBooks = ({ id }) => {
       {books
         .filter((book) => {
           if (id) {
-            return book.rating === 5 && book.id != id;
+            return book.rating === 5 && book.id !== id;
           }
           return book.rating === 5;
         })
